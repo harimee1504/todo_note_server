a3c30851f2b39ff708f10457d40ebb484839937a
harikrishnan <harimee1504@gmail.com>
2025-02-24 09:50:02 +0530

    typedef changes

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,5 +1,21 @@
-import { loadFilesSync } from '@graphql-tools/load-files'
+import fs from 'fs';
+import path from 'path';
+import { mergeTypeDefs } from "@graphql-tools/merge";
 
-const typeDefs = loadFilesSync('./**/*.graphql')
+const graphqlFilePaths = [   
+    './dist/graphql/note-comments/index.graphql',
+    './dist/graphql/notes/index.graphql',
+    './dist/graphql/todo-comments/index.graphql',
+    './dist/graphql/todos/index.graphql',
+    './dist/graphql/tags/index.graphql',
+    './dist/graphql/users/index.graphql'
+]
+
+const parsedGraphqlFiles = graphqlFilePaths.map(filePath => {
+    const file = fs.readFileSync(path.join(process.cwd(), filePath), 'utf-8');
+    return file;
+});
+
+const typeDefs = mergeTypeDefs(parsedGraphqlFiles);
 
 export default typeDefs;
